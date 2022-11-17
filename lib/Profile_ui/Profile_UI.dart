92d71d53f84e8908411d4de5c97f033a976829a7
Sahal <sahalsaidu123@gmail.com>
2022-11-17 13:12:51 +0530

    Profile UI

--- lib/Profile_ui/Profile_UI.dart
+++ lib/Profile_ui/Profile_UI.dart
@@ -8,11 +8,55 @@ class Profile_UI extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    List options = const [
+      [
+        Icon(
+          Icons.privacy_tip_outlined,
+          color: Colors.black,
+        ),
+        'Privacy'
+      ],
+      [
+        Icon(
+          Icons.history_rounded,
+          color: Colors.black,
+        ),
+        'Purchase History'
+      ],
+      [
+        Icon(
+          Icons.help_outline_rounded,
+          color: Colors.black,
+        ),
+        'Help & Support'
+      ],
+      [
+        Icon(
+          Icons.settings_outlined,
+          color: Colors.black,
+        ),
+        'Settings'
+      ],
+      [
+        Icon(
+          Icons.person_add_alt_outlined,
+          color: Colors.black,
+        ),
+        'Invite a friend'
+      ],
+      [
+        Icon(
+          Icons.logout_rounded,
+          color: Colors.black,
+        ),
+        'Logout'
+      ]
+    ];
     return Scaffold(
       extendBodyBehindAppBar: true,
       body: Container(
         color: Colors.grey[400],
-        child: ListView(
+        child: Column(
           children: [
             Container(
               height: 40,
@@ -40,32 +84,76 @@ class Profile_UI extends StatelessWidget {
                 ],
               ),
             ),
-            SizedBox(height: 15,),
+            const SizedBox(height: 15,),
             Center(
               child: Container(
                 height: 150,
                 width: 150,
                 decoration: BoxDecoration(
                   color: Colors.black,
-                  image: DecorationImage(fit: BoxFit.cover,image: NetworkImage('https://c4.wallpaperflare.com/wallpaper/22/74/535/hulk-wallpaper-preview.jpg')),
+                  image: const DecorationImage(fit: BoxFit.cover,image: NetworkImage('https://i.pinimg.com/originals/16/03/be/1603be4cb51b438b883cc1267e62354f.jpg')),
                   borderRadius: BorderRadius.circular(100),
                 ),
               ),
             ),
             Container(
-              margin: EdgeInsets.only(left: 70,right: 70,top: 10),
+              margin: const EdgeInsets.only(left: 70,right: 70,top: 10),
               height: 40,
               child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Icon(FontAwesomeIcons.squareFacebook,color: Colors.blue[900],size: 40,),
-                  Icon(FontAwesomeIcons.squareInstagram,color: Colors.green[900],size: 40,),
-                  Icon(FontAwesomeIcons.squareTwitter,color: Colors.blue[500],size: 40,),
-                  Icon(FontAwesomeIcons.squareWhatsapp,color: Colors.green[900],size: 40,),
+                  Icon(FontAwesomeIcons.squareFacebook,color: Colors.blue[900],size: 40,),const SizedBox(width: 5,),
+                  Icon(FontAwesomeIcons.squareTwitter,color: Colors.blue[500],size: 40,),const SizedBox(width: 5,),
+                  Icon(FontAwesomeIcons.squareYoutube,color: Colors.red[900],size: 40,),const SizedBox(width: 5,),
                   Icon(FontAwesomeIcons.squareWhatsapp,color: Colors.green[900],size: 40,),
                 ],
               ),
-            )
+            ),
+            const SizedBox(height:15),
+            const Center(
+              child:Text("Bruce Banner",style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold),
+            ),
+            ),
+            const Center(
+              child:Text("@bruce",style: TextStyle(fontSize: 20),
+              ),
+            ),
+            const SizedBox(height: 5,),
+            const Center(
+              child:Text("App Developer and Global Hero",style: TextStyle(fontSize: 20),
+              ),
+            ),
+            const SizedBox(height: 15,),
+            Expanded(
+                child: ListView.separated(
+                  separatorBuilder: (context, index) {
+                    return const SizedBox(
+                      height: 10,
+                    );
+                  },
+                  physics: const BouncingScrollPhysics(),
+                  itemCount: options.length,
+                  padding: const EdgeInsets.only(left: 30, right: 30, bottom: 40),
+                  itemBuilder: (context, index) {
+                    return Container(
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(40),
+                          color: Colors.black12.withOpacity(0.05)),
+                      child: ListTile(
+                        leading: options[index][0],
+                        title: Text(
+                          options[index][1],
+                          style: const TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.w500),
+                        ),
+                        trailing: const Icon(
+                          Icons.chevron_right_rounded,
+                          color: Colors.black,
+                        ),
+                      ),
+                    );
+                  },
+                ))
           ],
         ),
       ),
