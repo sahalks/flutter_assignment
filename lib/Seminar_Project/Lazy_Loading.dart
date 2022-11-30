740d3f5fd91ead5104e6009d4a91967c941759be
Sahal <sahalsaidu123@gmail.com>
2022-11-30 21:53:40 +0530

    Lazy Loading

---
+++ lib/Seminar_Project/Lazy_Loading.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class LazyLoading extends StatefulWidget {
+  const LazyLoading({super.key});
+
+  @override
+  _LazyLoadingState createState() {
+    return _LazyLoadingState();
+  }
+}
+
+class _LazyLoadingState extends State<LazyLoading> {
+  late List myList;
+  final ScrollController _scrollController = ScrollController();
+  int _currentMax = 10;
+
+  @override
+  void initState() {
+    super.initState();
+    myList = List.generate(10, (i) => "Number : ${i + 1}");
+    _scrollController.addListener(() {
+      if (_scrollController.position.pixels ==
+          _scrollController.position.maxScrollExtent) {
+        _getMoreData();
+      }
+    });
+  }
+
+  _getMoreData() {
+    for (int i = _currentMax; i < _currentMax + 10; i++) {
+      myList.add("Number : ${i + 1}");
+    }
+
+    _currentMax = _currentMax + 10;
+
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text("Lazy Loading"),
+      ),
+      body: ListView.builder(
+        controller: _scrollController,
+        itemExtent: 100,
+        itemBuilder: (context, i) {
+          if (i == myList.length) {
+            return const CupertinoActivityIndicator();
+          }
+          return ListTile(
+            title: Text(myList[i]),
+          );
+        },
+        itemCount: myList.length + 1,
+      ),
+    );
+  }
+}
---
+++ lib/Seminar_Project/Lazy_main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_assignment/Seminar_Project/Lazy_Loading.dart';
+import 'package:lazy_load_scrollview/lazy_load_scrollview.dart';
+
+
+void main(){
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: LazyLoading(),
+  ));
+}
